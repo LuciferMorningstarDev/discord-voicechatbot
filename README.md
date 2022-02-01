5dc0d3e3f655af89ad32b8db0aa50540776a4bbe
LuciferMorningstarDev <contact@lucifer-morningstar.xyz>
2022-02-01 16:04:17 +0100

    README update and add credits command

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 ## NodeJS Discord VoiceChat Bot COMING SOON
 
-[Invite](https://bit.ly/discord-voicechatbot) | [Dicordserver / Support](https://discord.gg/RZbDJTncSa)
+[Invite](https://bit.ly/discord-voicechatbot) | [Dicordserver / Support](https://bit.ly/discord-voicechatbot-support) | [SourceCode](https://bit.ly/discord-voicechatbot-repo)
 
 #### This discordbot allows you to enable temporary voice channels at your server
+
+---
+
+#### Clone Repository and run the bot
+
+-   > git clone git@github.com:LuciferMorningstarDev/discord-voicechatbot.git
+-   > cd discord-voicechatbot/
+-   Edit .env-example and save it as .env and configs/\*.json configurations
+-   > npm install
+-   If you want to use pm2 as your process manager
+    -   > npm install -g pm2
+    -   > pm2 start
+-   If you run your bot with plain nodejs command
+    -   > node bot.js
+    -   OR
+    -   > npm start
+
+---
+
+discord-voicechatbot | Copyright (c) 2022 | [LuciferMorningstarDev](https://github.com/LuciferMorningstarDev) | Licensed under the MIT License
+
+A copy of the LICENSE should be in project folder.
---
+++ slash_commands/credits.js
@@ -0,0 +1,55 @@
+/*
+ * discord-voicechatbot | Copyright (c) 2022 | LuciferMorningstarDev
+ * Licensed under the MIT License
+ *
+ * Repository: https://github.com/LuciferMorningstarDev/discord-voicechatbot
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * You should have received a copy of the MIT License
+ *
+ * Contact:
+ *  github: https://github.com/LuciferMorningstarDev
+ *  email: contact@lucifer-morningstar.xyz
+ *
+ * @author LuciferMorningstarDev - https://github.com/LuciferMorningstarDev
+ * @since 01.02.2022
+ *
+ */
+'use strict'; // https://www.w3schools.com/js/js_strict.asp
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports.run = async (bot, interaction, settings, lang = 'en_us') => {
+    const Discord = moduleRequire('discord.js');
+    try {
+        var embed = await bot.tools.generateEmbed({
+            title: 'discord-voicechatbot',
+            url: 'https://bit.ly/discord-voicechatbot',
+            color: 0x096069,
+            thumbnail: bot.user.displayAvatarURL(),
+            description: `
+[Invite](https://bit.ly/discord-voicechatbot) | [Support](https://bit.ly/discord-voicechatbot-support) | [SourceCode](https://bit.ly/discord-voicechatbot-repo)
+
+**Developed by:** [LuciferMorningstarDev](https://github.com/LuciferMorningstarDev)
+            `,
+            timestamp: false,
+        });
+        interaction.reply({ embeds: [embed] });
+    } catch (error) {
+        bot.error('Error in Slash Command Credits', error);
+    }
+};
+
+module.exports.data = (bot, lang = 'en_us') => {
+    var slashCommandData = new SlashCommandBuilder().setName('credits').setDescription('Credits of the bot: Invite / SourceCode / Support Invite');
+    return slashCommandData.toJSON();
+};
+
+module.exports.active = true;
